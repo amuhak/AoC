45f43c67354a3688db12721e7680d963d0032643
Amulya Jain <amuly.jain@gmail.com>
2023-12-04 11:22:21 -0500

    Day4 part2

--- src/Day4/Day4.java
+++ src/Day4/Day4.java
@@ -1,13 +1,16 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Day4 {
+    public static ArrayList<Long> answers = new ArrayList<>(200);
+    public static long ans = 0;
+
     public static void main(String[] args) throws Exception {
         BufferedReader r = new BufferedReader(new FileReader("input.txt"));
         StringTokenizer st;
-        long ans = 0;
         HashSet<Integer> set = new HashSet<>();
         String line;
         while ((line = r.readLine()) != null) {
@@ -28,12 +31,24 @@ public class Day4 {
                     ans1++;
                 }
             }
-            if (ans1 != 0) {
-                System.out.println(Math.pow(2, ans1 - 1));
-                ans += (long) Math.pow(2, ans1 - 1);
-            }
+            answers.add(ans1 != 0 ? (long) ans1 : 0L);
             set.clear();
         }
+        for (int i = 0; i < answers.size(); i++) {
+            ans += part2(i);
+        }
         System.out.println(ans);
+    }
+
+    public static int part2(int a) {
+        int ans1 = 1;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= answers.get(a); i++) {
+            list.add(i + a);
+        }
+        for (int l : list) {
+            ans1 += part2(l);
+        }
+        return ans1;
     }
 }
