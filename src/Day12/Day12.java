ba4b7cb1a0a70f8439ef79faa254df9e03281ce8
Amulya Jain <amuly.jain@gmail.com>
2023-12-12 19:16:26 -0500

    Day12 Part2 😭😭😭 (At least it's fast)

--- src/Day12/Day12.java
+++ src/Day12/Day12.java
@@ -4,31 +4,25 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Day12 {
+    public static HashMap<String, Long> dp;
+
     public static void main(String[] args) throws Exception {
         long ans = 0;
-        ArrayList<Pair<ArrayList<Character>, ArrayList<Integer>>> data =
-                getPairs();
+        ArrayList<Pair<ArrayList<Character>, ArrayList<Integer>>> data = getPairs();
         for (Pair<ArrayList<Character>, ArrayList<Integer>> pair : data) {
-            HashSet<String> temp1 = new HashSet<>();
-            // Create a bit mask
-            for (long i = 0; i < (1L << pair.getRight().getLast()); i++) {
-                ArrayList<Character> row = getCharacters(pair, i);
-                // Check if the row is valid
-                StringBuilder s2 = new StringBuilder();
-                row.forEach(s2::append);
-                if (isValid(s2.toString(), (ArrayList<Integer>) pair.getRight().clone())) {
-                    temp1.add(s2.toString());
-                }
-            }
-            ans += temp1.size();
+            dp = new HashMap<>();
+            ans += recursion(pair.getLeft(), pair.getRight(), 0, 0, 0);
+            //System.out.println(a++);
         }
         System.out.println(ans);
     }
 
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    // Read input
     private static ArrayList<Pair<ArrayList<Character>, ArrayList<Integer>>> getPairs() throws IOException {
         BufferedReader r = new BufferedReader(new FileReader("input.txt"));
         ArrayList<Pair<ArrayList<Character>, ArrayList<Integer>>> data = new ArrayList<>();
@@ -37,57 +31,85 @@ public class Day12 {
         while ((s = r.readLine()) != null) {
             ArrayList<Character> row = new ArrayList<>();
             ArrayList<Integer> row2 = new ArrayList<>();
-            int noOfUnknowns = 0;
             for (char c : s.split(" ")[0].toCharArray()) {
                 if (c == ' ') {
                     break;
                 }
                 row.add(c);
-                if (c == '?') {
-                    noOfUnknowns++;
-                }
+            }
+            ArrayList copy = (ArrayList) row.clone();
+            for (int i = 0; i < 4; i++) {
+                row.add('?');
+                row.addAll(copy);
             }
             StringTokenizer st = new StringTokenizer(s.split(" ")[1], ",");
             while (st.hasMoreTokens()) {
                 row2.add(Integer.parseInt(st.nextToken()));
             }
-            row2.add(noOfUnknowns);
+            copy = (ArrayList) row2.clone();
+            for (int i = 0; i < 4; i++) {
+                row2.addAll(copy);
+            }
+            // optimize
+            while (row.getFirst() == '.') {
+                row.removeFirst();
+            }
+            while (row.getLast() == '.') {
+                row.removeLast();
+            }
+            for (int i = 0; i < row.size() - 1; i++) {
+                if (row.get(i) == '.' && row.get(i + 1) == '.') {
+                    row.remove(i);
+                    i--;
+                }
+            }
             Pair<ArrayList<Character>, ArrayList<Integer>> pair = Pair.create(row, row2);
             data.add(pair);
         }
         return data;
     }
 
-    private static ArrayList<Character> getCharacters(Pair<ArrayList<Character>, ArrayList<Integer>> pair, long i) {
-        StringBuilder s1 = new StringBuilder(Long.toBinaryString(i));
-        while (s1.length() < pair.getRight().getLast()) {
-            s1.insert(0, "0");
-        }
-        // Replace the unknowns with the bit mask
-        ArrayList<Character> row = new ArrayList<>(pair.getLeft());
-        int temp = 0;
-        for (int j = 0; j < row.size(); j++) {
-            if (row.get(j) == '?') {
-                row.set(j, s1.charAt(temp++) == '0' ? '#' : '.');
-            }
-        }
-        return row;
-    }
-
-    public static boolean isValid(String s, ArrayList<Integer> constraints) {
-        constraints.removeLast();
-        StringTokenizer st = new StringTokenizer(s, ".");
-
-        while (st.hasMoreTokens()) {
-            if (constraints.isEmpty()) {
-                return false;
-            }
-            if (st.nextToken().length() == constraints.getFirst()) {
-                constraints.removeFirst();
+    public static long recursion(ArrayList<Character> data, ArrayList<Integer> constraint, int whereInData,
+                                 int whereInConstraint, int howMany) {
+        if (whereInData == data.size()) {
+            if (whereInConstraint == constraint.size() && howMany == 0) {
+                return 1;
+            } else if (whereInConstraint == constraint.size() - 1 && howMany == constraint.get(whereInConstraint)) {
+                return 1;
             } else {
-                return false;
+                return 0;
             }
         }
-        return constraints.isEmpty();
+        if (dp.containsKey(whereInData + " " + whereInConstraint + " " + howMany)) {
+            return dp.get(whereInData + " " + whereInConstraint + " " + howMany);
+        }
+        long ans = 0;
+        if (data.get(whereInData) == '.' && howMany == 0) {
+            ans += recursion(data, constraint, whereInData + 1, whereInConstraint, howMany);
+        } else if (data.get(whereInData) == '.'
+                && whereInConstraint < constraint.size()
+                && howMany == constraint.get(whereInConstraint)) {
+            ans += recursion(data, constraint, whereInData + 1, whereInConstraint + 1, 0);
+        } else if (data.get(whereInData) == '#') {
+            ans += recursion(data, constraint, whereInData + 1, whereInConstraint, howMany + 1);
+        } else if (data.get(whereInData) == '?') {
+            if (howMany == 0) {
+                //Emulate '.'
+                ans += recursion(data, constraint, whereInData + 1, whereInConstraint, howMany);
+            } else if (whereInConstraint < constraint.size()
+                    && howMany == constraint.get(whereInConstraint)) {
+                //Emulate '.' but you have also completed the constraint
+                ans += recursion(data, constraint, whereInData + 1, whereInConstraint + 1, 0);
+            }
+            //else you have failed
+
+            // now for '#'
+            ans += recursion(data, constraint, whereInData + 1, whereInConstraint, howMany + 1);
+        }
+
+        dp.put(whereInData + " " + whereInConstraint + " " + howMany, ans);
+        return ans;
     }
+
+
 }
