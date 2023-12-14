c92c8a0eb5f0b24b07bcc3720b0dcaf16b714c0e
Amulya Jain <amuly.jain@gmail.com>
2023-12-14 17:55:04 -0500

    Day14 Part2

--- src/Day14/Day14.java
+++ src/Day14/Day14.java
@@ -1,12 +1,19 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class Day14 {
     public static byte[][] grid;
 
     public static void main(String[] args) throws Exception {
+        HashSet<String> visited = new HashSet<>();
+        LinkedList<String> queue = new LinkedList<>();
         BufferedReader r = new BufferedReader(new FileReader("input.txt"));
         ArrayList<ArrayList<Byte>> temp = new ArrayList<>();
         String s;
@@ -29,11 +36,46 @@ public class Day14 {
         grid = new byte[temp.size()][temp.getFirst().size()];
         IntStream.range(0, temp.size()).forEach(i -> IntStream.range(0, temp.getFirst().size()).forEach(j ->
                 grid[i][j] = temp.get(i).get(j)));
-        fallNorth();
-        System.out.println(countNorth());
+        int cycleLength = 0;
+        String cycleStart = "";
+        while (true) {
+            fall();
+            rotateMatrix();
+            fall();
+            rotateMatrix();
+            fall();
+            rotateMatrix();
+            fall();
+            rotateMatrix();
+            if (visited.contains(Arrays.deepToString(grid.clone()))) {
+                if (cycleLength == 0) {
+                    cycleStart = (Arrays.deepToString(grid.clone()) + countNorth());
+                } else if (cycleStart.equals(Arrays.deepToString(grid.clone()) + countNorth())) {
+                    break;
+                }
+                cycleLength++;
+            } else {
+                cycleLength = 0;
+            }
+            visited.add(Arrays.deepToString(grid.clone()));
+            queue.add(Arrays.deepToString(grid.clone()) + countNorth());
+        }
+        long l = 0;
+        for (var s1 : queue) {
+            l++;
+            if (s1.equals(cycleStart)) {
+                break;
+            }
+        }
+        //\d{1,}[0-9]
+        String ans = queue.get((int) ((1000000000 - l) % cycleLength) + (int) l - 1);
+        Pattern pattern = Pattern.compile("\\d+[0-9]");
+        Matcher matcher = pattern.matcher(ans);
+        matcher.find();
+        System.out.println(matcher.group());
     }
 
-    public static void fallNorth() {
+    public static void fall() {
         for (int i = 0; i < grid[0].length; i++) {
             int base = 0;
             for (int j = 0; j < grid.length; j++) {
@@ -54,10 +96,27 @@ public class Day14 {
     public static long countNorth() {
         long ans = 0;
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 ans += grid[i][j] == 2 ? grid.length - i : 0;
             }
         }
         return ans;
     }
+
+    public static void rotateMatrix() {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        // Create a new matrix to store the rotated values
+        byte[][] rotatedMatrix = new byte[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                // Rotate 90 degrees clockwise formula
+                rotatedMatrix[j][rows - 1 - i] = grid[i][j];
+            }
+        }
+        grid = rotatedMatrix;
+    }
+
 }
