5f755949047a820c26bedf0c014b47562dd0f1da
Amulya Jain <amuly.jain@gmail.com>
2024-12-01 00:08:38 -0500

    d1 p1

--- src/2024/Day1/Day1.java
+++ src/2024/Day1/Day1.java
@@ -1,5 +1,24 @@
-public class Day1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Day1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("input.txt"));
+        long ans = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list1 = new ArrayList<>();
+
+        while (sc.hasNext()) {
+            list.add(sc.nextInt());
+            list1.add(sc.nextInt());
+        }
+        list.sort(Integer::compareTo);
+        list1.sort(Integer::compareTo);
+        for (int i = 0; i < list.size(); i++) {
+            ans += Math.abs(list.get(i) - list1.get(i));
+        }
+        System.out.println(ans);
     }
 }
