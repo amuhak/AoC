fa31d76d67a68c57ff39c7962b5281ef5453628e
Amulya Jain <amuly.jain@gmail.com>
2024-12-03 00:25:53 -0500

    d3 p2

--- src/2024/Day3/Day3.java
+++ src/2024/Day3/Day3.java
@@ -8,14 +8,22 @@ public class Day3 {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner sc = new Scanner(new File("Input.txt"));
         String in = sc.nextLine();
-        var match = Pattern.compile("mul\\([0-9]{1,3},[0-9]{1,3}\\)");
+        var match = Pattern.compile("(mul\\([0-9]{1,3},[0-9]{1,3}\\)|don't\\(\\)|do\\(\\))");
         Matcher m = match.matcher(in);
         long ans = 0;
+        boolean use = true;
         while (m.find()) {
-            String[] temp = m.group().substring(4, m.group().length() - 1).split(",");
-            long a = Long.parseLong(temp[0]);
-            long b = Long.parseLong(temp[1]);
-            ans += a * b;
+            var temp = m.group();
+            if (temp.equals("do()")) {
+                use = true;
+            } else if (temp.equals("don't()")) {
+                use = false;
+            } else if (use) {
+                String[] t1 = m.group().substring(4, m.group().length() - 1).split(",");
+                long a = Long.parseLong(t1[0]);
+                long b = Long.parseLong(t1[1]);
+                ans += a * b;
+            }
         }
         System.out.println(ans);
     }
