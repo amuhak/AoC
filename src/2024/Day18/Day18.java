2258dc2638abd187b0606db6de0fc984ea306a39
Amulya Jain <amuly.jain@gmail.com>
2024-12-18 19:41:19 -0500

    d18 p2

--- src/2024/Day18/Day18.java
+++ src/2024/Day18/Day18.java
@@ -8,42 +8,55 @@ public class Day18 {
     final public static int SIZE = 70 + 1;
 
     public static void main(String[] args) throws Exception {
+        long start = System.currentTimeMillis();
         try (var s = Files.lines(Path.of("Input.txt"))) {
-            bytes = s.map(line -> Arrays.stream(line.split(",")).mapToInt(Integer::parseInt).toArray()).toArray(int[][]::new);
+            bytes = s.map(line -> Arrays.stream(line.split(",")).mapToInt(Integer::parseInt).toArray())
+                    .toArray(int[][]::new);
         }
         System.out.println(Arrays.deepToString(bytes));
-        boolean[][] visited = new boolean[SIZE][SIZE];
+        boolean[][] copy = new boolean[SIZE][SIZE];
         for (int i = 0; i < 1024; i++) {
             var b = bytes[i];
-            visited[b[1]][b[0]] = true;
+            copy[b[1]][b[0]] = true;
         }
-        Arrays.stream(visited).map(Arrays::toString).forEach(System.out::println);
         // A* algorithm
-        PriorityQueue<Node> q = new PriorityQueue<>();
-        visited[0][0] = true;
-        q.add(new Node(0, 0, 0));
-        while (!q.isEmpty()) {
-            Node n = q.poll();
-            int x = n.x;
-            int y = n.y;
-            if (x == SIZE - 1 && y == SIZE - 1) {
-                System.out.println(n.cost);
-                return;
+        copy[0][0] = true;
+        boolean[][] visited = new boolean[SIZE][SIZE];
+        for (int l = 1024; l < bytes.length; l++) {
+            boolean itWorks = false;
+            copy[bytes[l][1]][bytes[l][0]] = true;
+            for (int i = 0; i < SIZE; i++) {
+                System.arraycopy(copy[i], 0, visited[i], 0, SIZE);
             }
-            for (var i : new int[][]{{x, y + 1}, {x, y - 1}, {x + 1, y}, {x - 1, y}}) {
-                int x1 = i[0];
-                int y1 = i[1];
-                if (x1 < 0 || x1 >= SIZE || y1 < 0 || y1 >= SIZE || visited[x1][y1]) {
-                    continue;
+            PriorityQueue<Node> q = new PriorityQueue<>();
+            q.add(new Node(0, 0, 0));
+            while (!q.isEmpty()) {
+                Node n = q.poll();
+                int x = n.x;
+                int y = n.y;
+                if (x == SIZE - 1 && y == SIZE - 1) {
+                    itWorks = true;
                 }
-                visited[x1][y1] = true;
-                q.add(new Node(x1, y1, n.cost + 1));
+                for (var i : new int[][]{{x, y + 1}, {x, y - 1}, {x + 1, y}, {x - 1, y}}) {
+                    int x1 = i[0];
+                    int y1 = i[1];
+                    if (x1 < 0 || x1 >= SIZE || y1 < 0 || y1 >= SIZE || visited[x1][y1]) {
+                        continue;
+                    }
+                    visited[x1][y1] = true;
+                    q.add(new Node(x1, y1, n.cost + 1));
+                }
+            }
+            if (!itWorks) {
+                System.out.println("No path found");
+                System.out.println(Arrays.toString(bytes[l]));
+                break;
             }
         }
-        Arrays.stream(visited).map(Arrays::toString).forEach(System.out::println);
+        System.out.println(System.currentTimeMillis() - start);
     }
 
-    static class Node implements Comparable<Node>{
+    static class Node implements Comparable<Node> {
         int x, y;
         double sum;
         int cost;
