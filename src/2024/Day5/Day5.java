3e058b983a03355616e675be8b084441cf41e24d
Amulya Jain <amuly.jain@gmail.com>
2024-12-25 00:31:41 -0500

    d5 p2

--- src/2024/Day5/Day5.java
+++ src/2024/Day5/Day5.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
 
 public class Day5 {
@@ -26,19 +27,39 @@ public class Day5 {
                 .parallel()
                 .map(i -> i.split(","))
                 .map(i -> Arrays.stream(i).map(Integer::parseInt).toList())
-                .mapToInt(a -> {
-                    for (int i = 1; i < a.size(); i++) {
-                        var set = arr.get(a.get(i));
-                        for (int j = 0; j < i; j++) {
-                            if (set.contains(a.get(j))) {
-                                return 0;
+                .filter(i -> !isValid(i, arr))
+                .map(ArrayList::new)
+                .mapToInt(i -> {
+                    while (!isValid(i, arr)) {
+                        // find the first invalid pair and swap them
+                        for (int j = 1; j < i.size(); j++) {
+                            var set = arr.get(i.get(j));
+                            for (int k = 0; k < j; k++) {
+                                if (set.contains(i.get(k))) {
+                                    int temp = i.get(j);
+                                    i.set(j, i.get(k));
+                                    i.set(k, temp);
+                                    break;
+                                }
                             }
                         }
                     }
-                    return a.get(a.size() / 2);
+                    return i.get(i.size() / 2);
                 })
                 .sum();
 
         System.out.println(ans);
+    }
+
+    public static boolean isValid(List<Integer> a, ArrayList<HashSet<Integer>> arr) {
+        for (int i = 1; i < a.size(); i++) {
+            var set = arr.get(a.get(i));
+            for (int j = 0; j < i; j++) {
+                if (set.contains(a.get(j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
