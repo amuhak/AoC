46cb107c97dc733c4744af204048a79e7f989c60
Jain, Amulya <ajain842@gatech.edu>
2025-12-03 03:16:31 -0500

    d3 p2

--- src/2025/Day3/Day3.java
+++ src/2025/Day3/Day3.java
@@ -1,34 +1,27 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.stream.IntStream;
-
-record IndexedChar(int value, int originalIndex) {
-}
 
 public class Day3 {
     public static void main(String[] args) throws FileNotFoundException {
         var start = System.nanoTime();
         var input = new BufferedReader(new FileReader("input.txt"));
-        System.out.println(input.lines()
-                .map(line -> IntStream.range(0, line.length())
-                        .mapToObj(i -> new IndexedChar(line.charAt(i) - '0', i))
-                        .sorted((a, b) -> -1 * Integer.compare(a.value(), b.value()))
-                        .toList())
-                .mapToInt(line -> {
-                    int out = line.getFirst().value() * 10;
-                    int idx = line.getFirst().originalIndex();
-                    for (var c : line) {
-                        if (c.originalIndex() > idx) {
-                            out += c.value();
-                            return out;
-                        }
-                    }
-                    out /= 10;
-                    out += line.get(1).value() * 10;
-                    return out;
-                })
-                .sum());
+        long sum = input.lines().mapToLong(line -> {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < line.length(); i++) {
+                char c = line.charAt(i);
+                while (!sb.isEmpty() && sb.charAt(sb.length() - 1) < c && line.length() - i > 12 - sb.length()) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+                if (sb.length() < 12) {
+                    sb.append(c);
+                }
+            }
+            long ans = Long.parseLong(sb.toString());
+            System.out.println(ans);
+            return ans;
+        }).sum();
+        System.out.println(sum);
         System.out.println("Time: " + (System.nanoTime() - start) / 1_000_000 + " ms");
     }
 }
