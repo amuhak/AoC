6f66bb2addba53a1dba973ad084dcd1a2fafba27
Jain, Amulya <ajain842@gatech.edu>
2025-12-10 17:58:18 -0500

    Day 10 p2

--- src/2025/Day10/Day10.java
+++ src/2025/Day10/Day10.java
@@ -1,8 +1,14 @@
-// Uses  org.choco-solver:choco-solver:5.0.0-beta.1
+// Uses io.github.tudo-aqua:z3-turnkey:RELEASE
 
-import org.chocosolver.solver.Model;
-import org.chocosolver.solver.Solution;
-import org.chocosolver.solver.variables.IntVar;
+import com.microsoft.z3.ArithExpr;
+import com.microsoft.z3.Context;
+import com.microsoft.z3.Expr;
+import com.microsoft.z3.IntExpr;
+import com.microsoft.z3.IntSort;
+import com.microsoft.z3.Model;
+import com.microsoft.z3.Optimize;
+import com.microsoft.z3.Status;
+import com.microsoft.z3.Z3Exception;
 
 void main() throws FileNotFoundException {
     var start = System.nanoTime();
@@ -42,48 +48,14 @@ void main() throws FileNotFoundException {
                 return new Machine(lightsArray, buttons, voltages);
             })
             .toList();
-    AtomicInteger num = new AtomicInteger();
-    int finalAns = input.parallelStream()
+
+    long finalAns = input.parallelStream()
             .map(Machine::toProblem)
-            .map(problem -> {
-                Model model = new Model("AoC Integer Solver");
-                int[][] A = problem.A;
-                int[] b = problem.b;
-                // int[] x = problem.x;
-                int numVars = A[0].length;
-                int numEqs = A.length;
-                IntVar[] x = model.intVarArray("x", numVars, 0, 500);
-                for (int row = 0; row < numEqs; row++) {
-                    // scalar product: A[row] * x = b[row]
-                    model.scalar(x, A[row], "=", b[row])
-                            .post();
-                }
-                IntVar cost = model.intVar("cost", 0, 500);
-                model.sum(x, "=", cost)
-                        .post();
-
-                model.setObjective(Model.MINIMIZE, cost);
-
-                // 6. Solve
-                Solution solution = model.getSolver()
-                        .findOptimalSolution(cost, Model.MINIMIZE);
-                if (solution != null) {
-                    System.out.println("Solution found!");
-                    System.out.println("Total Cost: " + solution.getIntVal(cost));
-                    for (int i = 0; i < numVars; i++) {
-                        System.out.printf("x[%d] = %d\n", i, solution.getIntVal(x[i]));
-                    }
-                    System.out.println("Done with " + (num.getAndIncrement()));
-                } else {
-                    System.out.println("No integer solution exists.");
-                }
-                return Arrays.stream(x)
-                        .mapToInt(i -> solution.getIntVal(i))
-                        .toArray();
-            })
-            .mapToInt(arr -> Arrays.stream(arr)
+            .map(Problem::solveWithZ3)
+            .mapToLong(solution -> Arrays.stream(solution)
                     .sum())
             .sum();
+
     System.out.println("Final Answer: " + finalAns);
     var end = System.nanoTime();
     System.out.println("Execution Time: " + (end - start) / 1_000_000 + " ms");
@@ -118,18 +90,84 @@ static class Machine {
                 A[i][j] = 1;
             }
         }
-        int[] x = new int[buttons.size()];
-        return new Problem(A, x, b);
+        return new Problem(A, b);
     }
 }
 
 static class Problem {
     int[][] A;
-    int[] x, b;
+    int[]  b;
 
-    Problem(int[][] A, int[] x, int[] b) {
+    Problem(int[][] A, int[] b) {
         this.A = A;
-        this.x = x;
         this.b = b;
+    }
+
+    /**
+     * Code by Gemini
+     * Solves the integer linear programming problem using Z3 SMT solver.
+     * Simply solves Ax = b with x >= 0 and minimizes the sum of x.
+     * A is the matrix that holds all the buttons.
+     * b is the vector that holds all the voltages (where we want to get to).
+     * x is the vector that holds the number of presses for each button to reach the voltages in b.
+     * x is unknown and to be solved for.
+     * @return An array of integers representing the solution, or an empty array if no solution exists.
+     */
+    int[] solveWithZ3() {
+        Problem problem = this;
+        int[][] A = problem.A;
+        int[] b = problem.b;
+        int numVars = A[0].length;
+        int numEqs = A.length;
+
+        try (Context ctx = new Context()) {
+            Optimize opt = ctx.mkOptimize();
+
+            IntExpr[] x = new IntExpr[numVars];
+            for (int i = 0; i < numVars; i++) {
+                x[i] = ctx.mkIntConst("x" + i);
+                opt.Add(ctx.mkGe(x[i], ctx.mkInt(0)));
+            }
+
+            // Constraints
+            for (int row = 0; row < numEqs; row++) {
+                ArithExpr<IntSort> rowSum = ctx.mkInt(0);
+                for (int col = 0; col < numVars; col++) {
+                    if (A[row][col] != 0) {
+                        ArithExpr<IntSort> term = ctx.mkMul(ctx.mkInt(A[row][col]), x[col]);
+                        rowSum = ctx.mkAdd(rowSum, term);
+                    }
+                }
+                opt.Add(ctx.mkEq(rowSum, ctx.mkInt(b[row])));
+            }
+
+            // Objective
+            ArithExpr<IntSort> totalCost = ctx.mkInt(0);
+            for (IntExpr var : x) {
+                totalCost = ctx.mkAdd(totalCost, var);
+            }
+
+            opt.MkMinimize(totalCost);
+
+            Status status = opt.Check();
+
+            if (status == Status.SATISFIABLE) {
+                Model model = opt.getModel();
+                int[] result = new int[numVars];
+
+                for (int i = 0; i < numVars; i++) {
+                    Expr<IntSort> val = model.eval(x[i], true);
+                    if (val.isIntNum()) {
+                        result[i] = Integer.parseInt(val.toString());
+                    }
+                }
+                return result;
+            } else {
+                return new int[0];
+            }
+        } catch (Z3Exception e) {
+            e.printStackTrace();
+            return new int[0];
+        }
     }
 }
