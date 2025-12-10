9cf81bfe648aeeff3b83ba6724f121528bb39b9f
Jain, Amulya <ajain842@gatech.edu>
2025-12-10 15:50:26 -0500

    d10 p2 test

--- src/2025/Day10/Day10.java
+++ src/2025/Day10/Day10.java
@@ -1,3 +1,7 @@
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solution;
+import org.chocosolver.solver.variables.IntVar;
+
 void main() throws FileNotFoundException {
     var start = System.nanoTime();
     var input = new BufferedReader(new FileReader("input.txt")).lines()
@@ -36,58 +40,94 @@ void main() throws FileNotFoundException {
                 return new Machine(lightsArray, buttons, voltages);
             })
             .toList();
-    System.out.println(input.stream()
-            .mapToInt(Machine::solve)
-            .sum());
+    AtomicInteger num = new AtomicInteger();
+    int finalAns = input.parallelStream()
+            .map(Machine::toProblem)
+            .map(problem -> {
+                Model model = new Model("AoC Integer Solver");
+                int[][] A = problem.A;
+                int[] b = problem.b;
+                // int[] x = problem.x;
+                int numVars = A[0].length;
+                int numEqs = A.length;
+                IntVar[] x = model.intVarArray("x", numVars, 0, 500);
+                for (int row = 0; row < numEqs; row++) {
+                    // scalar product: A[row] * x = b[row]
+                    model.scalar(x, A[row], "=", b[row])
+                            .post();
+                }
+                IntVar cost = model.intVar("cost", 0, 500);
+                model.sum(x, "=", cost)
+                        .post();
+
+                model.setObjective(Model.MINIMIZE, cost);
+
+                // 6. Solve
+                Solution solution = model.getSolver()
+                        .findOptimalSolution(cost, Model.MINIMIZE);
+                if (solution != null) {
+                    System.out.println("Solution found!");
+                    System.out.println("Total Cost: " + solution.getIntVal(cost));
+                    for (int i = 0; i < numVars; i++) {
+                        System.out.printf("x[%d] = %d\n", i, solution.getIntVal(x[i]));
+                    }
+                    System.out.println("Done with " + (num.getAndIncrement()));
+                } else {
+                    System.out.println("No integer solution exists.");
+                }
+                return Arrays.stream(x)
+                        .mapToInt(i -> solution.getIntVal(i))
+                        .toArray();
+            })
+            .mapToInt(arr -> Arrays.stream(arr)
+                    .sum())
+            .sum();
+    System.out.println("Final Answer: " + finalAns);
     var end = System.nanoTime();
     System.out.println("Execution Time: " + (end - start) / 1_000_000 + " ms");
 }
 
 static class Machine {
-    int lights;
-    ArrayList<Integer> buttons;
+    boolean[] lights;
+    ArrayList<ArrayList<Integer>> buttons;
     ArrayList<Integer> voltages;
 
     Machine(boolean[] lights, ArrayList<ArrayList<Integer>> buttons, ArrayList<Integer> voltages) {
-        int lightsBitmask = 0;
-        for (int i = 0; i < lights.length; i++) {
-            if (lights[i]) {
-                lightsBitmask |= 1 << i;
-            }
-        }
-        this.lights = lightsBitmask;
-        ArrayList<Integer> Buttons = new ArrayList<>();
-        for (var button : buttons) {
-            int b = 0;
-            for (var light : button) {
-                b |= 1 << light;
-            }
-            Buttons.add(b);
-        }
-        this.buttons = Buttons;
+        this.lights = lights;
+        this.buttons = buttons;
         this.voltages = voltages;
     }
 
     @Override
     public String toString() {
-        return "Machine{" + "lights=" + Integer.toBinaryString(lights) + ", buttons=" + buttons + ", voltages="
-                + voltages + '}';
+        return "Machine{" + "lights=" + Arrays.toString(lights) + ", buttons=" + buttons + ", voltages=" + voltages
+                + '}';
     }
 
-    int solve() {
-        int min = Integer.MAX_VALUE;
-        int steps = buttons.size();
-        for (int i = 0; i < 1 << steps; i++) {
-            int num = 0;
-            for (int j = 0; j < steps; j++) {
-                if ((i & (1 << j)) != 0) {
-                    num ^= buttons.get(j);
-                }
-            }
-            if (num == lights) {
-                min = Math.min(min, Integer.bitCount(i));
+    Problem toProblem() {
+        int[] b = new int[voltages.size()];
+        for (int i = 0; i < voltages.size(); i++) {
+            b[i] = voltages.get(i);
+        }
+        int[][] A = new int[voltages.size()][buttons.size()];
+        for (int j = 0; j < buttons.size(); j++) {
+            ArrayList<Integer> button = buttons.get(j);
+            for (int i : button) {
+                A[i][j] = 1;
             }
         }
-        return min;
+        int[] x = new int[buttons.size()];
+        return new Problem(A, x, b);
+    }
+}
+
+static class Problem {
+    int[][] A;
+    int[] x, b;
+
+    Problem(int[][] A, int[] x, int[] b) {
+        this.A = A;
+        this.x = x;
+        this.b = b;
     }
 }
