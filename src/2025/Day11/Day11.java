d715af6ff285872cba55edaab03ee203e0f747b5
Jain, Amulya <ajain842@gatech.edu>
2025-12-11 22:23:51 -0500

    Day 11 p2

--- src/2025/Day11/Day11.java
+++ src/2025/Day11/Day11.java
@@ -8,7 +8,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class Day11 {
     static ConcurrentHashMap<String, ArrayList<String>> adjList = new ConcurrentHashMap<>();
-    static ConcurrentHashMap<String, Integer> visited = new ConcurrentHashMap<>();
+    static ConcurrentHashMap<String, Long> visited = new ConcurrentHashMap<>();
 
     static void main() throws FileNotFoundException {
         var start = System.nanoTime();
@@ -19,23 +19,38 @@ public class Day11 {
                     ArrayList<String> value = new ArrayList<>(List.of(Arrays.copyOfRange(arr, 1, arr.length)));
                     adjList.put(arr[0], value);
                 });
-        int ans = dfs("you");
+        long InToA = dfs("svr", "dac");
+        reset();
+        long AToB = dfs("dac", "fft");
+        reset();
+        long BToOut = dfs("fft", "out");
+        reset();
+        long InToB = dfs("svr", "fft");
+        reset();
+        long BToA = dfs("fft", "dac");
+        reset();
+        long AToOut = dfs("dac", "out");
+        long ans = InToA * AToB * BToOut + InToB * BToA * AToOut;
         var end = System.nanoTime();
         System.out.println("Final Answer: " + ans);
         System.out.println("Execution Time: " + (end - start) / 1_000_000 + " ms");
     }
 
-    static int dfs(String node) {
-        if (visited.getOrDefault(node, -1) != -1) {
+    static void reset() {
+        visited.clear();
+    }
+
+    static long dfs(String node, String out) {
+        if (visited.getOrDefault(node, -1L) != -1) {
             return visited.get(node);
         }
 
-        int count = 0;
+        long count = 0;
         for (String neighbor : adjList.getOrDefault(node, new ArrayList<>())) {
-            if (neighbor.equals("out")) {
+            if (neighbor.equals(out)) {
                 count++;
             } else {
-                count += dfs(neighbor);
+                count += dfs(neighbor, out);
             }
         }
         visited.put(node, count);
